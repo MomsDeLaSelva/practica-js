9dac63c888dddd4cb91ef7facd43dfbe2ac08393
Moms <reocampo88@gmail.com>
2025-08-12 01:45:34 -0300

    ejercicio 7

--- TP1/tp1.js
+++ TP1/tp1.js
@@ -17,17 +17,27 @@ let num2 = parseFloat(prompt('Ingrese otro número'))
 document.write(num1, ' + ', num2, ' = ', num1+num2) */
 
 //Ejercicio 6
-/* let num1 = parseFloat(prompt('Ingrese un número'))
-let num2 = parseFloat(prompt('Ingrese otro número')) 
-if (num1 > num2) {
-    document.write('El ', num1, ' es el número más gande.')
-}else if (num1 < num2){
-    document.write('El ', num2, ' es el número más gande.')
+/* let num3 = parseFloat(prompt('Ingrese un número'))
+let num4 = parseFloat(prompt('Ingrese otro número')) 
+if (num3 > num4) {
+    document.write('El ', num3, ' es el número más gande.')
+}else if (num3 < num4){
+    document.write('El ', num4, ' es el número más gande.')
 }else{
     document.write('los númeron ingresados son iguales.')
 } */
 
 //Ejercicio 7
+/* let num5 = parseFloat(prompt('Ingrese un número'))
+let num6 = parseFloat(prompt('Ingrese un segundo número')) 
+let num7 = parseFloat(prompt('Ingrese un tercer número')) 
+if (num5 > num6 && num5 >num7) {
+    document.write('El ', num5, ' es el número más gande.')
+}else if (num6 > num7){
+    document.write('El ', num6, ' es el número más gande.')
+}else{
+    document.write('El ', num7, ' es el número más gande.')
+} */
 
 //Ejercicio 8
 
