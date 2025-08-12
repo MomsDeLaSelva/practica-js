0036beff676f4397ce2c56a12694d6b983026dfc
Moms <reocampo88@gmail.com>
2025-08-12 09:43:41 -0300

    ejercicio 9

--- TP1/tp1.js
+++ TP1/tp1.js
@@ -8,47 +8,59 @@
 /* document.write(3 + 5) */
 
 //Ejercicio 4
-/* let nombreUsuario = prompt('Escriba su nombre de usuario')
-document.write('Hola ' + nombreUsuario) */
+/* let nombreUsuario = prompt('Escriba su nombre de usuario');
+document.write('Hola ' + nombreUsuario); */
 
 //Ejercicio 5
-/* let num1 = parseFloat(prompt('Ingrese un número'))
-let num2 = parseFloat(prompt('Ingrese otro número'))
-document.write(num1, ' + ', num2, ' = ', num1+num2) */
+/* let num1 = parseFloat(prompt('Ingrese un número'));
+let num2 = parseFloat(prompt('Ingrese otro número'));
+document.write(num1, ' + ', num2, ' = ', num1+num2); */
 
 //Ejercicio 6
-/* let num3 = parseFloat(prompt('Ingrese un número'))
-let num4 = parseFloat(prompt('Ingrese otro número')) 
+/* let num3 = parseFloat(prompt('Ingrese un número'));
+let num4 = parseFloat(prompt('Ingrese otro número')) ;
 if (num3 > num4) {
-    document.write('El ', num3, ' es el número más gande.')
+    document.write('El ', num3, ' es el número más gande.');
 }else if (num3 < num4){
-    document.write('El ', num4, ' es el número más gande.')
+    document.write('El ', num4, ' es el número más gande.');
 }else{
-    document.write('los númeron ingresados son iguales.')
+    document.write('los númeron ingresados son iguales.');
 } */
 
 //Ejercicio 7
-/* let num5 = parseFloat(prompt('Ingrese un número'))
-let num6 = parseFloat(prompt('Ingrese un segundo número')) 
-let num7 = parseFloat(prompt('Ingrese un tercer número')) 
+/* let num5 = parseFloat(prompt('Ingrese un número'));
+let num6 = parseFloat(prompt('Ingrese un segundo número'));
+let num7 = parseFloat(prompt('Ingrese un tercer número'));
 if (num5 > num6 && num5 >num7) {
-    document.write('El ', num5, ' es el número más gande.')
+    document.write('El ', num5, ' es el número más gande.');
 }else if (num6 > num7){
-    document.write('El ', num6, ' es el número más gande.')
+    document.write('El ', num6, ' es el número más gande.');
 }else{
-    document.write('El ', num7, ' es el número más gande.')
+    document.write('El ', num7, ' es el número más gande.');
 } */
 
 //Ejercicio 8
-/* let divisible = parseFloat(prompt('Ingrese un número'))
+/* let divisible = parseFloat(prompt('Ingrese un número'));
 if (divisible % 2 == 0){
-    document.write(divisible, ' es divisible por 2.')
+    document.write(divisible, ' es divisible por 2.');
 }else{
-    document.write(divisible, ' no es divisible por 2.')
+    document.write(divisible, ' no es divisible por 2.');
 } */
 
 //Ejercicio 9
+/* let frase = prompt('Escriba una frase').toLowerCase();
+let vocales = "";
+
+for (i=0; i<frase.length; i++){
+    let letra = frase.charAt(i);
+    if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+        vocales += letra;
+    }
+}
+
+document.write(vocales); */
 
 //Ejercicio 10
 
-//Ejercicio 11
+//Ejercicio 11hola
+
