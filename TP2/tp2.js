d5e968da284e02054ab78cb7cc3d227c7795dcf1
Moms <reocampo88@gmail.com>
2025-08-12 15:44:59 -0300

    ejercicio 1

---
+++ TP2/tp2.js
@@ -0,0 +1,42 @@
+//Ejercicio 1
+/* let edad = Number(prompt('Ingrese su edad'));
+
+if (edad <=0 || edad >=90) {
+    document.write('El número ingresado es inválido. Por favor ingrese una edad válida.');
+} else if (edad >= 18){
+    document.write('Usted ya puede conducir.');
+} else {
+    document.write('Usted no puede conducir.');
+} */
+
+//Ejercicio 2
+
+//Ejercicio 3
+
+//Ejercicio 4
+
+//Ejercicio 5
+
+//Ejercicio 6
+
+//Ejercicio 7
+
+//Ejercicio 8
+
+//Ejercicio 9
+
+//Ejercicio 10
+
+//Ejercicio 11
+
+//Ejercicio 12
+
+//Ejercicio 13
+
+//Ejercicio 14
+
+//Ejercicio 15
+
+//Ejercicio 16
+
+//Ejercicio 17
