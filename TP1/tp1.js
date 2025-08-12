4cd969e676f82fdbcf66cefed1620ce5bba57c52
Moms <reocampo88@gmail.com>
2025-08-12 01:35:32 -0300

    ejercicio 6

--- TP1/tp1.js
+++ TP1/tp1.js
@@ -17,6 +17,15 @@ let num2 = parseFloat(prompt('Ingrese otro número'))
 document.write(num1, ' + ', num2, ' = ', num1+num2) */
 
 //Ejercicio 6
+/* let num1 = parseFloat(prompt('Ingrese un número'))
+let num2 = parseFloat(prompt('Ingrese otro número')) 
+if (num1 > num2) {
+    document.write('El ', num1, ' es el número más gande.')
+}else if (num1 < num2){
+    document.write('El ', num2, ' es el número más gande.')
+}else{
+    document.write('los númeron ingresados son iguales.')
+} */
 
 //Ejercicio 7
 
