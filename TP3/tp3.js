d901adb3600318ca9a712c067a8c470ccc860c75
Moms <reocampo88@gmail.com>
2025-08-26 00:38:38 -0300

    ejercicio 1

---
+++ TP3/tp3.js
@@ -0,0 +1,18 @@
+//Ejercicio 1
+let meses =["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+document.write('Lista de meses:');
+for (i = 0; i < meses.length; i++) {
+    document.write(`<p>. ${meses[i]}</p>`)
+}
+
+//Ejercicio 2
+
+//Ejercicio 3
+
+//Ejercicio 4
+
+//Ejercicio 5
+
+//Ejercicio 6
+
+//Ejercicio 7
